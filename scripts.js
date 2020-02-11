6c5258cb7fe9676613e649472feb21f77d6f4aeb
Sam Kautz <samkautz@yahoo.com>
2020-02-11 00:03:27 -0600

    Changed li styling to display row and cleaned up javascript

--- scripts.js
+++ scripts.js
@@ -1,38 +1,35 @@
 function addTodo() {
 	let node = document.createElement('li');
-	node.setAttribute('id', 'list-item');
+	node.setAttribute('class', 'list-item');
 	let text = document.getElementById('todo-input').value;
-	let textnode = document.createTextNode(text);
-	node.appendChild(textnode);
+	let textnode = document.createElement('span');
+	textnode.innerText = text;
+	//node.appendChild(textnode);
 
 	let deleteButton = document.createElement('button');
 	let deleteButtonX = document.createTextNode('X');
 	deleteButton.appendChild(deleteButtonX);
+	deleteButton.addEventListener('click', removeNode);
 
 	let StrikeThroughRadio = document.createElement('input');
 	StrikeThroughRadio.setAttribute('type', 'checkbox');
 	StrikeThroughRadio.addEventListener('click', function(e) {
-		if (StrikeThroughRadio.checked === true) {
-			e.target.nextElementSibling.style.textDecoration = 'line-through';
-			e.target.nextElementSibling.style.color = 'red';
-		} else {
-			e.target.nextElementSibling.style.textDecoration = 'none';
-			e.target.nextElementSibling.style.color = 'black';
-		}
+		StrikeThroughRadio.checked
+			? e.target.nextElementSibling.classList.add('strike-text')
+			: e.target.nextElementSibling.classList.remove('strike-text');
 	});
 
-	deleteButton.addEventListener('click', function() {
-		let deleteLI = document.getElementById('display');
-		deleteLI.removeChild(node);
-		let RemoveDeleteButton = document.getElementById('display');
-		RemoveDeleteButton.removeChild(deleteButton);
-		let RemoveStrikeThroughRadio = document.getElementById('display');
-		RemoveStrikeThroughRadio.removeChild(StrikeThroughRadio);
-	});
+	node.appendChild(StrikeThroughRadio);
+	node.appendChild(textnode);
+	node.appendChild(deleteButton);
 
-	document.getElementById('display').appendChild(StrikeThroughRadio);
+	//document.getElementById('display').appendChild(StrikeThroughRadio);
 	document.getElementById('display').appendChild(node);
-	document.getElementById('display').appendChild(deleteButton);
+	//document.getElementById('display').appendChild(deleteButton);
+}
+
+function removeNode(e) {
+	e.target.parentElement.remove();
 }
 
 function ClearInputOnSubmit() {
