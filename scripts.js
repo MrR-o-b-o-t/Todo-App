1e0b2b39194d7aed402e527a96e825b684366ed2
Sam Kautz <samkautz@yahoo.com>
2020-02-12 15:44:16 -0600

    Add functions to prevent blank form submit and clear input field on submit.

--- scripts.js
+++ scripts.js
@@ -2,15 +2,14 @@ function addTodo() {
 	let node = document.createElement('li');
 	node.setAttribute('class', 'list-item');
 	let text = document.getElementById('todo-input').value;
+	if (text.length < 1) return;
 	let textnode = document.createElement('span');
 	textnode.innerText = text;
-	//node.appendChild(textnode);
 
 	let deleteButton = document.createElement('button');
 	let deleteButtonX = document.createTextNode('X');
 	deleteButton.appendChild(deleteButtonX);
 	deleteButton.addEventListener('click', removeNode);
-
 	let StrikeThroughRadio = document.createElement('input');
 	StrikeThroughRadio.setAttribute('id', 'checkbox-styles');
 	StrikeThroughRadio.setAttribute('type', 'checkbox');
@@ -24,34 +23,7 @@ function addTodo() {
 	node.appendChild(textnode);
 	node.appendChild(deleteButton);
 
-	//document.getElementById('display').appendChild(StrikeThroughRadio);
 	document.getElementById('display').appendChild(node);
-
-	(function() {
-		'use strict';
-		window.addEventListener(
-			'load',
-			function() {
-				// Fetch all the forms we want to apply custom Bootstrap validation styles to
-				var forms = document.getElementsByClassName('needs-validation');
-				// Loop over them and prevent submission
-				var validation = Array.prototype.filter.call(forms, function(form) {
-					form.addEventListener(
-						'submit',
-						function(event) {
-							if (form.checkValidity() === false) {
-								event.preventDefault();
-								event.stopPropagation();
-							}
-							form.classList.add('was-validated');
-						},
-						false
-					);
-				});
-			},
-			false
-		);
-	})();
 }
 
 function removeNode(e) {
