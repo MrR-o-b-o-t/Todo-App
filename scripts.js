ecd97efd049e53e62337f357aee1cad47d1343b5
Sam Kautz <samkautz@yahoo.com>
2020-02-12 18:17:33 -0600

    Update checkbox styles.

--- scripts.js
+++ scripts.js
@@ -5,11 +5,12 @@ function addTodo() {
 	if (text.length < 1) return;
 	let textnode = document.createElement('span');
 	textnode.innerText = text;
-
+	// Create delete todo button
 	let deleteButton = document.createElement('button');
 	let deleteButtonX = document.createTextNode('X');
 	deleteButton.appendChild(deleteButtonX);
 	deleteButton.addEventListener('click', removeNode);
+	// Create strike through radio input
 	let StrikeThroughRadio = document.createElement('input');
 	StrikeThroughRadio.setAttribute('id', 'checkbox-styles');
 	StrikeThroughRadio.setAttribute('type', 'checkbox');
